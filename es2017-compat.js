025c25d4fbd5a328f7926ceb8f65b668faeda6fb
Fritz Lin <uxfritz@163.com>
2022-10-19 01:18:27 +0800

    specify both `env` and `parserOptions.ecmaVersion`; remove unnecessary globals

--- es2017-compat.js
+++ es2017-compat.js
@@ -1,7 +1,10 @@
 module.exports = {
   env: {
-    es2017: true, // equivalent to `es8`
+    es2017: true, // es8/es2017
     // overrides standard
     es2021: false,
-  }
+  },
+  parserOptions: {
+    ecmaVersion: 8 // es8/es2017
+  },
 }
