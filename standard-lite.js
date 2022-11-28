fa3d73b800a97408da87b060639e0b4f4584ba8e
Fritz Lin <uxfritz@163.com>
2022-11-28 12:49:41 +0800

    more rules for standard-lite

--- standard-lite.js
+++ standard-lite.js
@@ -4,7 +4,15 @@ module.exports = {
     'indent': [2, 2],
     // semi
     'semi': [2, 'never'],
+    'semi-spacing': [2, { before: false, after: true }],
+    'semi-style': [2, 'first'],
     // quote
     'quotes': [2, 'single'],
+    // space
+    'keyword-spacing': 2,
+    'space-before-blocks': [2, 'always'],
+    'space-in-parens': [2, 'never'],
+    'space-infix-ops': 2,
+    'space-unary-ops': [2, { words: true, nonwords: false }],
   },
 }
